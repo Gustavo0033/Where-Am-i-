b6e0d115e2ffadc8f8bcc6b2edc7b50292cb4334
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2023-05-15 21:04:54 -0300

    Where am i

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapa: MKMapView!
+    
+    var gerenciadorLocal = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        gerenciadorLocal.delegate = self
+        gerenciadorLocal.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocal.requestWhenInUseAuthorization()
+        gerenciadorLocal.startUpdatingLocation()
+       
 
 
+}
+
+    // como ver o usuario se movendo
+func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    
+    let localizacaoUsuario: CLLocation = locations.last!
+    
+    let latitude: CLLocationDegrees = localizacaoUsuario.coordinate.latitude
+    let longitude: CLLocationDegrees = localizacaoUsuario.coordinate.longitude
+
+    let deltaLatitude: CLLocationDegrees = 0.01
+    let deltaLongitude: CLLocationDegrees = 0.01
+    
+    let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+    
+    
+    
+    let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
+    
+    let regiao: MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
+    
+    mapa.setRegion(regiao, animated: true)
+}
 }
 
